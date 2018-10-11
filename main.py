76f3494915fe672883e037afb3d96cd6a07caaec
kayakIYD <ficupaolo@gmail.com>
2018-10-11 12:08:21 +0200

    Add second stock-to-predict
    Make code more flexible and clearer

--- main.py
+++ main.py
@@ -21,22 +21,29 @@ start_date = "2010-01-01"
 today = str(datetime.date.today())
 
 print('Acquire data...')
+data_list = []
+dates_list = []
+
 xxx = quandl.get("xxx", start_date=start_date, end_date=today, collapse="monthly", returns="pandas")
-data = xxx['Open']
+data_list.append(xxx['Open'])
+dates_list.append(data_to_date(xxx.reset_index()['Date']))
 
-print('Fit data...')
-x, models = modeling.fit_data(np.reshape(np.arange(0, len(data.values)), (len(data.values), 1)), data.values)
+yyy = quandl.get("yyy", start_date=start_date, end_date=today, collapse="monthly",
+                         returns="pandas")
+data_list.append(yyy['local_price'])
+dates_list.append(data_to_date(yyy.reset_index()['Date']))
 
-print('Predict data...')
-predictions = modeling.predict_data(x, models)
+for data, dates in zip(data_list, dates_list):
+    print('Fit data...')
+    x, models = modeling.fit_data(np.reshape(np.arange(0, len(data.values)), (len(data.values), 1)), data.values)
 
-print('Plot data...')
-dates = []
-date_mod = data.reset_index()['Date']
-for value in date_mod:
-    dates.append(str(value)[:10])
+    print('Predict data...')
+    predictions = modeling.predict_data(x, models)
 
-plotting.plot_data(dates, data.values, predictions, models)
+    print('Plot data...')
 
-print('Done!')
+    plotting.plot_data(dates, data.values, predictions, models)
 
+    print('Done for set of data for one company')
+
+print('All actions done!')
