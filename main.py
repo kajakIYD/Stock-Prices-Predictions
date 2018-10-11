3170c596a9d2c24413be628297be2ee07ee3e537
kayakIYD <ficupaolo@gmail.com>
2018-10-11 10:57:23 +0200

    Move modeling stuff to modeling module

--- main.py
+++ main.py
@@ -2,50 +2,11 @@
 # (so it is kind of approximation)
 
 import quandl
+import modeling
 import plotting
 import numpy as np
-from sklearn.svm import SVR   # Support Vector Machine -> Support Vector Regression used for prediction
-# could be use for regression as well
+
 import datetime
-
-
-def fit_data(x, y):
-    print('Set up models...')
-    svr_lin = SVR(kernel='linear', C=1e3)
-    svr_poly = SVR(kernel='poly', C=1e3, degree=1) # the same as linear:p
-    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-
-    print('Fit models...')
-    models = [svr_lin, svr_poly, svr_rbf]
-    for model in models:
-        print(str(model.kernel) + "...")
-        model.fit(x, y)
-
-    return [x, models]
-
-
-def predict_data(x, models):
-    predictions = []
-    for model in models:
-        predictions.append(model.predict(x))
-    return predictions
-
-
-def plot_data(x, reference, predictions, models):
-    plt.scatter(x, reference, s=1, marker=',', color='black', label='Reference Data')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('Approximation/prediction model comparison')
-
-    for model, prediction in zip(models, predictions):
-        plt.plot(x, prediction, label=str(model.kernel))
-
-    labels = x[:10]
-    plt.xticks(range(0, len(x), 10), labels, rotation='vertical')
-    tmp = range(0, len(x), 10)
-
-    plt.legend()
-    plt.show()
 
 
 print("Let's play!")
@@ -57,10 +18,10 @@ xxx = quandl.get("xxx", start_date=start_date, end_date=today, collapse="monthly
 data = xxx['Open']
 
 print('Fit data...')
-x, models = fit_data(np.reshape(np.arange(0, len(data.values)), (len(data.values), 1)), data.values)
+x, models = modeling.fit_data(np.reshape(np.arange(0, len(data.values)), (len(data.values), 1)), data.values)
 
 print('Predict data...')
-predictions = predict_data(x, models)
+predictions = modeling.predict_data(x, models)
 
 print('Plot data...')
 dates = []
---
+++ modeling.py
@@ -0,0 +1,25 @@
+# Module holding all modeling-stuff
+
+from sklearn.svm import SVR   # Support Vector Machine -> Support Vector Regression used for prediction
+# could be use for regression as well
+
+
+def predict_data(x, models):
+    predictions = []
+    for model in models:
+        predictions.append(model.predict(x))
+    return predictions
+
+
+def fit_data(x, y):
+    print('Set up models...')
+    svr_lin = SVR(kernel='linear', C=1e3)
+    svr_poly = SVR(kernel='poly', C=1e3, degree=1) # the same as linear:p
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+
+    print('Fit models...')
+    models = [svr_lin, svr_poly, svr_rbf]
+    for model in models:
+        print(str(model.kernel) + "...")
+        model.fit(x, y)
+    return [x, models]
